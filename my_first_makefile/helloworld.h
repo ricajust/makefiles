866ca1aae78c0d4a351279d4ef0101b445293740
Ricajust <ricardojustino@fastmail.com>
2022-06-20 08:35:57 -0300

    {rda-silv}[makefile] - create a first and second examples

---
+++ my_first_makefile/helloworld.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helloworld.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/19 15:29:33 by rda-silv          #+#    #+#             */
+/*   Updated: 2022/06/19 16:02:58 by rda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HELLOWORLD_H
+# define HELLORWORLD_H
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * @brief a simple function, where print "Hello world" in screen
+ * 
+ */
+void	helloworld(void);
+
+#endif
---
+++ my_second_makefile/helloworld.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helloworld.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-silv <rda-silv@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/19 17:31:54 by rda-silv          #+#    #+#             */
+/*   Updated: 2022/06/19 17:33:54 by rda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HELLOWORLD_H
+# define HELLOWORLD_H
+# include <stdio.h>
+
+/**
+ * @brief a simple hello world function
+ * 
+ */
+void	helloworld(void);
+
+#endif
